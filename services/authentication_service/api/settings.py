3cb171faffe31885a766c8e7b973388a14510000
keroo <kerooadel5@gmail.com>
2024-03-30 21:16:56 +0200

    add files

--- services/authentication_service/api/settings.py
+++ services/authentication_service/api/settings.py
@@ -104,20 +104,20 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        # 'HOST': 'authentication_db',
-        'HOST': 'localhost',
-    
-        'PORT': '5432',
-        'NAME': 'baggr_auth',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     # 'HOST': 'authentication_db',
+    #     'HOST': 'localhost',
+    
+    #     'PORT': '5432',
+    #     'NAME': 'baggr_auth',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '123456',
+    # }
 }
 
 REST_FRAMEWORK = {
